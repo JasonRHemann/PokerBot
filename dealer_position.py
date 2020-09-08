52270d3928b38adfbb63bf60cf8b765b1bea707b
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-09-07 23:26:21 -0500

    Started working on Player count

--- dealer_position.py
+++ dealer_position.py
@@ -3,27 +3,25 @@ import threading
 import time
 
 # Dealer positions for 6 handed desktop
-# dealer_position = (388, 508)
-# dealer_position1 = (156, 515)
-# dealer_position2 = (156, 363)
-# dealer_position3 = (407, 305)
-# dealer_position4 = (529, 363)
-# dealer_position5 = (529, 515)
+dealer_position1 = (388, 508)
+dealer_position2 = (156, 515)
+dealer_position3 = (156, 363)
+dealer_position4 = (407, 305)
+dealer_position5 = (529, 363)
+dealer_position6 = (529, 515)
 
 # Dealer postions for 6 handed laptop
-dealer_position1 = (442, 552)
-dealer_position2 = (220, 559)
-dealer_position3 = (220, 413)
-dealer_position4 = (460, 358)
-dealer_position5 = (576, 413)
-dealer_position6 = (576, 559)
+# dealer_position1 = (442, 552)
+# dealer_position2 = (220, 559)
+# dealer_position3 = (220, 413)
+# dealer_position4 = (460, 358)
+# dealer_position5 = (576, 413)
+# dealer_position6 = (576, 559)
 
 dealer_list = [dealer_position1, dealer_position2, dealer_position3,
                dealer_position4, dealer_position5, dealer_position6]
 
 # threading.Timer(5.0, dealer_seat).start()
-
-new_dealer = 7
 
 
 # class Dealer:
@@ -40,25 +38,25 @@ new_dealer = 7
 # print(dealer)
 
 
-class Dealer:
-    def __init__(self, seat):
-        self.seat = seat
+# class Dealer:
+#     def __init__(self, seat):
+#         self.seat = seat
 
-    def __repr__(self):
-        return "Dealer is in seat {}".format(self.seat)
+#     def __repr__(self):
+#         return "Dealer is in seat {}".format(self.seat)
 
-    def dealer_seat(self):
+#     def dealer_seat():
 
-        #threading.Timer(5.0, dealer_seat).start()
+#         # threading.Timer(5.0, dealer_seat).start()
 
-        for idx, val in enumerate(dealer_list):
-            im = pyautogui.screenshot()
-            button = im.getpixel(val)
-            if(button[0] > 200):
-                dealer = idx
-                return (Dealer(dealer))
+#         for idx, val in enumerate(dealer_list):
+#             im = pyautogui.screenshot()
+#             button = im.getpixel(val)
+#             if(button[0] > 200):
+#                 dealer = idx
+#                 print(dealer)
 
-
+#     dealer_seat()
 # class Watcher:
 #     def __init__(self, value):
 #         self.variable = value
@@ -92,3 +90,17 @@ class Dealer:
 # print(emp_1.first)
 # print(emp_1.email)
 # print(emp_1.fullname())
+
+# def dealer_seat():
+
+#     threading.Timer(5.0, dealer_seat).start()
+
+#     for idx, val in enumerate(dealer_list):
+#         im = pyautogui.screenshot()
+#         button = im.getpixel(val)
+#         if(button[0] > 200):
+#             dealer = idx
+#             print(dealer)
+
+
+# dealer_seat()
